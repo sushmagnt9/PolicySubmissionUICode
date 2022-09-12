b3212b4889fc117abd1c91fac071d1f87d5ac2bb
sushmagnt9 <sushmagnt9@gmail.com>
2022-09-12 18:19:32 +0530

    css

--- src/app/service/app-routing.module.ts
+++ src/app/service/app-routing.module.ts
@@ -6,7 +6,7 @@ import { MemberComponent } from '../member/member.component';
 import { MembersearchComponent } from '../membersearch/membersearch.component';
 import { RegisterComponent } from '../register/register.component';
 
-const routes: Routes = [{path : 'login' , component : LoginComponent},
+const routes: Routes = [{path : '' , component : LoginComponent},
 {path : 'register' , component : RegisterComponent},
 {path : 'member' , component : MemberComponent},
 {path : 'memberSearch' , component : MembersearchComponent},
